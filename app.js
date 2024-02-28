46008d342adb30991d29d81fd475bbfd2e786231
g5fighter <grmapal2@gmail.com>
2024-02-28 14:36:13 +0100

    ✍️ Updated Docs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 
 import {
-    IgApiClient,
-    IgLoginTwoFactorRequiredError
+  IgApiClient,
+  IgLoginTwoFactorRequiredError
 } from 'instagram-private-api';
+
+const DEBUG = false;
 
 const ig = new IgApiClient();
 import fsNorm from 'fs';
@@ -11,70 +13,99 @@ const fs = fsNorm.promises;
 import inquirer from 'inquirer';
 import Bluebird from 'bluebird';
 
-// Calcular previamente el numero de esperas
 // Configure the app
-var loggedUser = '';
-var userToSearch = [];
+var loggedUser = ''; // The user who is logged in
+var userToSearch = []; // The users we want to search
+
 // if -1 we get all the following
 // if -2 we get all the following but divided in tiers -> 5,10,50,100,500,1000 in k
-var followerLimit; 
-const timeMargin = 6;
-const listPath = "";
-const listFileName = "followers_";
+var followerLimit;
+const followerLimits = [5000, 10000, 50000, 100000, 500000, Infinity]; // The follower limits for the tiers
 
-const exhaustiveMode = 5;
+const timeMargin = 6; // The time margin for the exhaustive mode
+const listPath = ""; // The path where the lists will be saved
+const listFileName = "followers_"; // The name of the file
 
-function SesionPath(){
-  return loggedUser+".json";
+const exhaustiveMode = 5; // The amount of times we will get the following list
+
+
+
+/**
+ * Returns the session path for the logged user.
+ * @returns {string} The session path.
+ */
+function SesionPath() {
+  return loggedUser + ".json";
 }
 
+/**
+ * Saves the data to a file or database.
+ * @param {any} data - The data to be saved.
+ */
 function Save(data) {
-  // here you would save it to a file/database etc.
-  fsNorm.writeFileSync(SesionPath(), JSON.stringify(data), function(err, result) {
-    if(err) console.error(err);
+  fsNorm.writeFileSync(SesionPath(), JSON.stringify(data), function (err, result) {
+    if (err) console.error(err);
   });
 }
 
+/**
+ * Checks if the data exists.
+ * @returns {boolean} Returns true if the data exists, otherwise false.
+ */
 async function Exists() {
-  // here you would check if the data exists
   try {
-    if(fsNorm.existsSync(SesionPath())){
-      const data = await fs.readFile(SesionPath(), 'utf8')
-      if(data==""){
-        return false;
-      }else{
-        return true;
-      }
-    }else{
+    if (!fsNorm.existsSync(SesionPath())) {
       return false;
     }
-  } catch(err) {
+
+    const data = await fs.readFile(SesionPath(), 'utf8');
+    return data != "";
+
+  } catch (err) {
     console.error(err)
     return false;
   }
 }
 
+/**
+ * Loads data from a file.
+ * @returns {Promise<Object>} The parsed JSON data from the file.
+ */
 async function Load() {
   const data = await fs.readFile(SesionPath(), 'utf8')
   // console.log(data)
-  return JSON.parse(data); 
+  return JSON.parse(data);
 }
 
+/**
+ * Tries to load a session and checks if it is still valid.
+ * @returns {Promise<boolean>} A promise that resolves to true if the session is valid, false otherwise.
+ */
 async function tryLoadSession() {
-  if (await Exists()) {
-    try {
-      await ig.state.deserialize(await Load());
-      // try any request to check if the session is still valid
-      await ig.account.currentUser();
-      return true;
-    } catch (e) {
-      return false;
+
+  if (!Exists()){
+    if (DEBUG) console.log("No session found");
+    return false;
+  }
+  try {
+    await ig.state.deserialize(await Load());
+    // try any request to check if the session is still valid
+    await ig.account.currentUser();
+    return true;
+  } catch (e) {
+    if (DEBUG) {
+      console.log("Error loading session");
+      console.log(e);
+      console.log(e.stack);
     }
-  }else{
     return false;
   }
 }
 
+/**
+ * Logs in the user.
+ * @returns {Promise<void>} A promise that resolves when the login process is complete.
+ */
 async function login() {
   console.log("|- Logging in");
   const {
@@ -83,7 +114,7 @@ async function login() {
     type: 'input',
     name: 'username',
     message: `Enter your username`,
-  }, ]);
+  },]);
   loggedUser = username;
   ig.state.generateDevice(username);
   ig.state.proxyUrl = process.env.IG_PROXY;
@@ -94,7 +125,7 @@ async function login() {
     Save(serialized);
   });
 
-  if(!(await tryLoadSession())) {
+  if (!(await tryLoadSession())) {
     const {
       password
     } = await inquirer.prompt([{
@@ -102,7 +133,7 @@ async function login() {
       mask: '*',
       name: 'password',
       message: `Enter your password`,
-    }, ]);
+    },]);
     // This call will provoke request.end$ stream
     return Bluebird.try(() => ig.account.login(username, password)).catch(
       IgLoginTwoFactorRequiredError,
@@ -122,7 +153,7 @@ async function login() {
           type: 'input',
           name: 'code',
           message: `Enter code received via ${verificationMethod === '1' ? 'SMS' : 'TOTP'}`,
-        }, ]);
+        },]);
         // Use the code to finish the login process
         return ig.account.twoFactorLogin({
           username,
@@ -132,9 +163,13 @@ async function login() {
           trustThisDevice: '1', // Can be omitted as '1' is used by default
         });
       },
-    ).catch(e => {console.log(e); console.log(e.stack); process.exit(1);});
+    ).catch(e => {
+      console.log(e);
+      if (DEBUG) console.log(e.stack);
+      process.exit(1);
+    });
     // Most of the time you don't have to login after loading the state
-  }else{
+  } else {
     console.log("|- Session loaded");
   }
 }
@@ -144,18 +179,18 @@ login().then(async () => {
   console.log('------------------------------------------------------');
   console.log("|- Introduce one by one all users you want to search");
 
-  do{
+  do {
     var {
       userEntered
     } = await inquirer.prompt([{
       type: 'input',
       name: 'userEntered',
       message: `Enter a user (press enter to stop)`,
-    }, ]);
-    if(userEntered!=""){
+    },]);
+    if (userEntered != "") {
       userToSearch.push(userEntered);
     }
-  }while(userEntered!="");
+  } while (userEntered != "");
 
   console.log('------------------------------------------------------');
   const {
@@ -164,17 +199,17 @@ login().then(async () => {
     type: 'input',
     name: 'follLimit',
     message: `Enter a follower limit (-1 is no limit, -2 is divided in tiers)`,
-  }, ]);
+  },]);
 
   //userToSearch.push
 
   followerLimit = parseInt(follLimit)
 
-  for(searchedUser in userToSearch){
+  for (let searchedUser in userToSearch) {
     console.log('-------------------------------------------------------');
-    console.log("|- Searching "+userToSearch[searchedUser]);
+    console.log("|- Searching " + userToSearch[searchedUser]);
     const targetUser = await ig.user.searchExact(userToSearch[searchedUser]);
-    
+
     console.log("|- Getting feed");
     const followingFeed = ig.feed.accountFollowing(targetUser.pk);
     const followersFeed = ig.feed.accountFollowers(targetUser.pk);
@@ -183,12 +218,12 @@ login().then(async () => {
     const following = await getAllItemsFromFeed(followingFeed);
 
     console.log(`Follower Count: ${followers.length} - Following Count: ${following.length}`);
-    
+
     const followersUsername = new Set(followers.map(({ username }) => username));
 
     let notFollowingYou;
     // Get to times the users feed
-    if(exhaustiveMode != 0){
+    if (exhaustiveMode != 0) {
       console.log("|- Exhaustive mode activated");
       // for from 0 to exhaustive mode
       var globalUsernamesSet = new Set([...followersUsername]);
@@ -201,122 +236,139 @@ login().then(async () => {
         timeAmount += time;
         times.push(time);
       }
-      timeAmount/=1000;
-      console.log("Estimated time: "+Math.trunc((timeAmount/60))+" minutes "+Math.trunc((timeAmount%60))+" seconds");
-      for(var i=0; i<exhaustiveMode; i++){
-        
-        console.log(`|- Getting feed ${(2+i)} - Remaining time: ${Math.trunc((timeAmount/60))} minutes ${Math.trunc((timeAmount%60))} seconds`);
+      timeAmount /= 1000;
+      console.log("Estimated time: " + Math.trunc((timeAmount / 60)) + " minutes " + Math.trunc((timeAmount % 60)) + " seconds");
+      for (var i = 0; i < exhaustiveMode; i++) {
+
+        console.log(`|- Getting feed ${(2 + i)} - Remaining time: ${Math.trunc((timeAmount / 60))} minutes ${Math.trunc((timeAmount % 60))} seconds`);
         await new Promise(resolve => setTimeout(resolve, times[i]));
         const followingFeed2 = ig.feed.accountFollowing(targetUser.pk);
         const followersFeed2 = ig.feed.accountFollowers(targetUser.pk);
-  
+
         const followers2 = await getAllItemsFromFeed(followersFeed2);
         const following2 = await getAllItemsFromFeed(followingFeed2);
 
         console.log(`Follower Count: ${followers2.length} - Following Count: ${following2.length}`);
 
         const followersUsername2 = new Set(followers2.map(({ username }) => username));
-      
+
         globalUsernamesSet = new Set([...globalUsernamesSet, ...followersUsername2]);
         globalAllFollowing = [...globalAllFollowing, ...following2];
-      
-        timeAmount-=(times[i]/1000);
+
+        timeAmount -= (times[i] / 1000);
       }
       // Filter the ones who are not following you
       notFollowingYou = globalAllFollowing.filter(({ username }) => !globalUsernamesSet.has(username));
 
-    }else{
-          // Filtering through the ones who aren't following you.
-      notFollowingYou = following.filter(({ username }) => !followersUsername.has(username));    
+    } else {
+      // Filtering through the ones who aren't following you.
+      notFollowingYou = following.filter(({ username }) => !followersUsername.has(username));
     }
 
     var myNeededUsers = new Map();
-    notFollowingYou.forEach(user =>  myNeededUsers.set(user.username,user.pk));
+    notFollowingYou.forEach(user => myNeededUsers.set(user.username, user.pk));
 
-    await GetFinalList(myNeededUsers,userToSearch[searchedUser]);
-  }  
-  });
-  
-  // This function will get the final list filtering users with more than followerLimit followers
-  async function GetFinalList(users,user){
-    // console.log("Getting final list");
-    // 5,10,50,100,500,1000 
-    var users5k = new Map();
-    var users10k = new Map();
-    var users50k = new Map();
-    var users100k = new Map();
-    var users500k = new Map();
-    var users1000k = new Map();
-    if(followerLimit != -1){
-        const iterator1 = users.entries();
-        var valIndex = 1;
-        var remainingItems = users.size;
-        
-        var times = [];
-        var timeAmount = 0;
-        for (var i = 0; i < remainingItems; i++) {
-          var time = Math.round(Math.random() * timeMargin * 1000) + 1000;
-          timeAmount += time;
-          times.push(time);
-        }
-        timeAmount/=1000;
-        console.log("Estimated time: "+Math.trunc((timeAmount/60))+" minutes "+Math.trunc((timeAmount%60))+" seconds");
-        var exitFromWhile = false;
-        while((val = iterator1.next().value)!=undefined || exitFromWhile){
-            console.log(`Checking: ${valIndex}/${remainingItems} - Remaining time: ${Math.trunc((timeAmount/60))} minutes ${Math.trunc((timeAmount%60))} seconds`);
-            valIndex++;
-            ig.user.info(val[1]).then(foll => {
-              if(followerLimit==-2){
-                if(foll.follower_count<=5000){
-                  users5k.set(val[0],val[1]);
-                }else if(foll.follower_count<=10000){
-                  users10k.set(val[0],val[1]);
-                }else if(foll.follower_count<=50000){
-                  users50k.set(val[0],val[1]);
-                }else if(foll.follower_count<=100000){
-                  users100k.set(val[0],val[1]);
-                }else if(foll.follower_count<=500000){
-                  users500k.set(val[0],val[1]);
-                }else{
-                  users1000k.set(val[0],val[1]);
-                }
-              }else{
-                if(foll.follower_count>followerLimit){
-                  users.delete(val[0]);
-                }
-              }
-              console.log(`${val[0]} -> Follower Count: ${foll.follower_count}`)
-            }).catch(error =>{
-              console.log(error);
-              exitFromWhile = true;
-            } );
-            timeAmount-=(times[valIndex-2]/1000);
-            await new Promise(resolve => setTimeout(resolve, times[valIndex-2]));
-        }
-    }
-    if(followerLimit==-2){
-      SaveResponse(users5k,user+"_5k");
-      SaveResponse(users10k,user+"_10k");
-      SaveResponse(users50k,user+"_50k");
-      SaveResponse(users100k,user+"_100k");
-      SaveResponse(users500k,user+"_500k");
-      SaveResponse(users1000k,user+"_1000k");
-    }else{
-      SaveResponse(users,user);  
-    }
-}
+    await GetFinalList(myNeededUsers, userToSearch[searchedUser]);
+  }
+});
 
-// This function will save the response
-async function SaveResponse(string,user){
-    const result = [...string.keys()].flat().join('\n')
-    var fileName = listPath+listFileName+user+".txt";
-    console.log("|- Saved in "+fileName);
-      fs.writeFile(fileName, result, function(err, result) {
-        if(err) console.error(err);
+
+/**
+ * Retrieves the final list filtering users with more than followerLimit followers
+ * @param {Map} users - The map of users.
+ * @param {string} user - The user.
+ * @returns {void}
+ */
+async function GetFinalList(users, user) {
+  // console.log("Getting final list");
+  // 5,10,50,100,500,1000 
+
+  const userMaps = followerLimits.map(() => new Map());
+
+  if (followerLimit != -1) {
+    const iterator1 = users.entries();
+    var valIndex = 1;
+    const remainingItems = users.size;
+
+    const times = Array.from({ length: remainingItems }, () => {
+      const time = Math.round(Math.random() * timeMargin * 1000) + 1000;
+      return time;
+    });
+    let timeAmount = times.reduce((a, b) => a + b, 0) / 1000;
+
+    console.log("Estimated time: " + Math.trunc((timeAmount / 60)) + " minutes " + Math.trunc((timeAmount % 60)) + " seconds");
+
+    let exitFromWhile = false;
+    let val;
+    while ((val = iterator1.next().value) != undefined || exitFromWhile) {
+      console.log(`Checking: ${valIndex}/${remainingItems} - Remaining time: ${Math.trunc((timeAmount / 60))} minutes ${Math.trunc((timeAmount % 60))} seconds`);
+      valIndex++;
+      ig.user.info(val[1]).then(foll => {
+        handleFollowerCount(foll, val, userMaps);
+      }).catch(error => {
+        console.log(error);
+        exitFromWhile = true;
       });
+      timeAmount -= (times[valIndex - 2] / 1000);
+      await new Promise(resolve => setTimeout(resolve, times[valIndex - 2]));
+    }
+  }
+  saveResponses(userMaps, user);
 }
 
-// This function will get all the items from a feed
+/**
+ * Handles the follower count of a user and performs actions based on the count.
+ * @param {Object} foll - The follower object containing the follower count.
+ * @param {Array} val - The value array containing user information.
+ * @param {Array<Map>} userMaps - The array of user maps.
+ */
+function handleFollowerCount(foll, val, userMaps) {
+  if (followerLimit == -2) {
+    const index = followerLimits.findIndex(limit => foll.follower_count <= limit);
+    userMaps[index].set(val[0], val[1]);
+  } else {
+    if (foll.follower_count > followerLimit) {
+      users.delete(val[0]);
+    }
+  }
+  console.log(`${val[0]} -> Follower Count: ${foll.follower_count}`);
+}
+
+/**
+ * Saves the responses for a given user.
+ * @param {Array} userMaps - An array of user maps.
+ * @param {string} user - The user name.
+ */
+function saveResponses(userMaps, user) {
+  if (followerLimit == -2) {
+    userMaps.forEach((map, index) => {
+      SaveResponse(map, `${user}_${followerLimits[index]}k`);
+    });
+  } else {
+    SaveResponse(users, user);
+  }
+}
+
+/**
+ * Saves the response string to a file.
+ * @param {string} string - The response string to be saved.
+ * @param {string} user - The user identifier.
+ * @returns {Promise<void>} - A promise that resolves when the file is saved successfully.
+ */
+async function SaveResponse(string, user) {
+  const result = [...string.keys()].flat().join('\n')
+  var fileName = listPath + listFileName + user + ".txt";
+  console.log("|- Saved in " + fileName);
+  fs.writeFile(fileName, result, function (err, result) {
+    if (err) console.error(err);
+  });
+}
+
+/**
+ * Retrieves all items from a feed.
+ * @param {Feed} feed - The feed to retrieve items from.
+ * @returns {Promise<Array>} - A promise that resolves to an array of items.
+ */
 async function getAllItemsFromFeed(feed) {
   let items = [];
   do {
